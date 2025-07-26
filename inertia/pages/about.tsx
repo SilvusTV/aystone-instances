57f30a5360d86c58979c54ff1bcb679ab9aee622
Silvus <hugo.combe.84@gmail.com>
2025-07-26 13:50:45 +0200

    feat: improve link styling in about page & improve build cmd

--- inertia/pages/about.tsx
+++ inertia/pages/about.tsx
@@ -5,10 +5,10 @@ export default function About() {
   return (
     <Layout>
       <Head title="À propos" />
-      
+
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl font-bold mb-6">À propos des instances d'Aystone2</h1>
-        
+
         <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6 mb-8">
           <h2 className="text-2xl font-semibold mb-4">À quoi sert ce site ?</h2>
           <p className="mb-4">
@@ -19,15 +19,15 @@ export default function About() {
             En tant que visiteur, vous pouvez parcourir tous les projets en cours et terminés, filtrer par dimension,
             par type de projet ou par instance, et découvrir les créations impressionnantes de notre communauté.
           </p>
-          <p>
+          <p className="mb-4">
             En tant que joueur inscrit, vous pouvez créer et gérer vos propres projets, en fournissant des détails
             comme les coordonnées, la dimension, et même un lien vers la dynmap pour que d'autres puissent facilement
             trouver et visiter vos constructions.
           </p>
-          <p>Ce site est inspiré du <a href="https://cobble.ultralion.xyz/"> siteweb de l'instance cobble</a> créé par le développeur et joueur <a
-            href="https://ultralion.xyz/"> UltraLion</a></p>
+          <p>Ce site est inspiré du <a href="https://cobble.ultralion.xyz/" className=" hover:text-primary-200 underline transition"> siteweb de l'instance cobble</a> créé par le développeur et joueur <a
+            href="https://ultralion.xyz/" className=" hover:text-primary-200 underline transition"> UltraLion</a></p>
         </div>
-        
+
         <div className="bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
           <h2 className="text-2xl font-semibold mb-4">Comment s'inscrire et rejoindre l'instance ?</h2>
           <div className="mb-6">
@@ -36,7 +36,7 @@ export default function About() {
               Commencez par vous inscrire sur ce site en utilisant votre pseudo Minecraft. Votre email est necessaire en cas de perte de votre mot de passe. Il ne sera pas utilisé à d'autres fins.
             </p>
           </div>
-          
+
           <div className="mb-6">
             <h3 className="text-xl font-medium mb-2">Étape 2: Validation de votre compte</h3>
             <p>
@@ -44,7 +44,7 @@ export default function About() {
               Si cette action n'est pas faite, vous resterez spectateur.
             </p>
           </div>
-          
+
           <div>
             <h3 className="text-xl font-medium mb-2">Étape 3: Profitez</h3>
             <p className="mb-2">
